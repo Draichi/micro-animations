185eaddd695c28251c973e3a1880c3a77f8edd55
@Draichi <lucasdraichi@gmail.com>
2022-08-08 04:15:35 -0300

    feat: notifications animation

--- app.js
+++ app.js
@@ -6,10 +6,36 @@ const tl = gsap.timeline({
 });
 
 const home = document.querySelector(".home");
+const notifications = document.querySelector(".notifications");
 
-gsap.set(".feather", {scale: 0, transformOrigin: "center"})
+gsap.set(".feather", { scale: 0, transformOrigin: "center" });
 home.addEventListener("click", () => {
-  gsap.fromTo(".home-svg", { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1});
-  gsap.fromTo(".feather", {y: -5, scale: 0}, {y: 20, scale: 1.5, duration: 1, stagger: 0.2})
-  gsap.fromTo(".right-feather", {x: 0}, {x: 5})
+  gsap.fromTo(".home-svg", { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1 });
+  gsap.fromTo(
+    ".feather",
+    { y: -5, scale: 0 },
+    { y: 20, scale: 1.5, duration: 1, stagger: 0.2 }
+  );
+  gsap.fromTo(".right-feather", { x: 0 }, { x: 5 });
+});
+
+gsap.set(".bell", { transformOrigin: "top center" });
+gsap.set(".ringer", { transformOrigin: "top center" });
+gsap.set(".wave", { opacity: 0, transformOrigin: 'bottom' });
+notifications.addEventListener("click", () => {
+  gsap.fromTo(
+    ".bell",
+    { rotation: -5 },
+    { rotation: 0, ease: "elastic.out(5, 0.2)", duration: 2 }
+  );
+  gsap.fromTo(
+    ".ringer",
+    { rotation: -3, x: -0.5 },
+    { rotation: 0, x: 0, ease: "elastic.out(5, 0.2)", duration: 1.2 }
+  );
+  gsap.fromTo(
+    ".wave",
+    { opacity: 1, scale: 0 },
+    { opacity: 0, scale: 1.3, duration: 1 }
+  );
 });
